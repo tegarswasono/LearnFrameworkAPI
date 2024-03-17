c5cc40d115f5b69a2336f5e2ec43e254dee0d7c1
tegarswasono <tegar.s@weefer.co.id>
2024-03-17 11:33:25 +0700

    add quasar

--- VueQuasarUI/VueQuasarUI/src/views/AboutView.vue
+++ VueQuasarUI/VueQuasarUI/src/views/AboutView.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page 99</h1>
+    <div class="q-pa-md q-gutter-sm">
+      <q-btn color="white" text-color="black" label="Standard" />
+      <q-btn color="primary" label="Primary" />
+      <q-btn color="secondary" label="Secondary" />
+      <q-btn color="amber" glossy label="Amber" />
+      <q-btn color="brown-5" label="Brown 5" />
+      <q-btn color="deep-orange" glossy label="Deep Orange" />
+      <q-btn color="purple" label="Purple" />
+      <q-btn color="black" label="Black" />
+    </div>
   </div>
 </template>
 
