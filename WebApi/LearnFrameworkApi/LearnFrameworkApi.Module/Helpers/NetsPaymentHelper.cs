e571ff8960f8624461d7eb9b7923b085f303f6b0
tegarswasono <tegar.s@weefer.co.id>
2024-03-21 21:01:24 +0700

    .

---
+++ WebApi/LearnFrameworkApi/LearnFrameworkApi.Module/Helpers/NetsPaymentHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnFrameworkApi.Module.Helpers
+{
+    public static class NetsPaymentHelper
+    {
+        public static string GenerateSignature(string txnReq, string secretKey)
+        {
+            string concatPayloadAndSecretKey = txnReq + secretKey;
+            return EncodeBase64(HashSHA256ToBytes(Encoding.UTF8.GetBytes(concatPayloadAndSecretKey)));
+        }
+        public static byte[] HashSHA256ToBytes(byte[] data)
+        {
+            byte[] bytes = SHA256.HashData(data);
+            return bytes;
+        }
+        public static string EncodeBase64(byte[] data)
+        {
+            return Convert.ToBase64String(data);
+        }
+    }
+}
