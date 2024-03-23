fd1a2f2f2b138468088230bfd86a614690ecaf6c
tegarswasono <tegar.s@weefer.co.id>
2024-03-23 15:54:39 +0700

    add serilog

---
+++ WebApi/LearnFrameworkApi/LearnFrameworkApi.Api/Controllers/Learn/LearnSerilogTestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace LearnFrameworkApi.Api.Controllers.Learn
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LearnSerilogTestController : ControllerBase
+    {
+        private readonly ILogger<LearnSerilogTestController> _logger;
+        public LearnSerilogTestController(ILogger<LearnSerilogTestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            _logger.LogInformation("info 2");
+            _logger.LogWarning("Warning 2");
+            _logger.LogError("Error 2");
+            return Ok("OKE");
+        }
+    }
+}
