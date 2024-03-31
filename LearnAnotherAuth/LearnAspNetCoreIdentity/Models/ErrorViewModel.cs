9743c8bf19d1742c3428fbd5066d2391db384ac6
tegarswasono <tegar.s@weefer.co.id>
2024-03-31 12:51:50 +0700

    add learn another auth

---
+++ LearnAnotherAuth/LearnAspNetCoreIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearnAspNetCoreIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
