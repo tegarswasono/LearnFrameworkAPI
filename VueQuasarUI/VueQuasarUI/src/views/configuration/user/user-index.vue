7021f8493fc85d2c309d59d62c2da358a8997abf
tegarswasono <tegar.s@weefer.co.id>
2024-05-03 16:01:40 +0700

    add dialog

--- VueQuasarUI/VueQuasarUI/src/views/configuration/user/user-index.vue
+++ VueQuasarUI/VueQuasarUI/src/views/configuration/user/user-index.vue
@@ -17,6 +17,7 @@ onMounted(async () => {
 
 //table
 const columns = [
+  { name: 'actions', label: '', align: 'left', style: 'width:50px;' },
   { name: 'email', label: 'Email', field: 'email', sortable: true, align: 'left' },
   { name: 'fullname', label: 'Fullname', field: 'fullName', sortable: true, align: 'left' },
   { name: 'isActive', label: 'Active', field: 'isActive', sortable: true, align: 'left' }
@@ -49,6 +50,9 @@ const OnRequest = async (props) => {
   pagination.value.rowsPerPage = rowsPerPage
   await getData()
 }
+
+//AddEdit
+const dialog = ref(false)
 </script>
 
 <template>
@@ -70,8 +74,60 @@ const OnRequest = async (props) => {
       @request="OnRequest"
     >
       <template v-slot:top>
-        <q-btn color="primary" icon="add" size="sm" label="Add" @click="addRow" />
+        <q-btn icon="add" size="sm" label="Add" color="primary" @click="dialog = true" />
+      </template>
+      <template v-slot:body-cell-actions="props">
+        <q-td :props="props">
+          <q-btn dense round flat color="grey" size="xs" icon="visibility"></q-btn>
+          <q-btn dense round flat color="grey" size="xs" icon="edit"></q-btn>
+          <q-btn dense round flat color="grey" size="xs" icon="delete"></q-btn>
+        </q-td>
       </template>
     </q-table>
+
+    <q-dialog v-model="dialog" persistent backdrop-filter="blur(4px) saturate(150%)">
+      <q-card style="width: 700px; max-width: 80vw">
+        <q-card-section>
+          <div class="text-h6">Add User</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+            <q-input
+              filled
+              v-model="name"
+              label="Your name *"
+              hint="Name and surname"
+              lazy-rules
+              :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+            />
+
+            <q-input
+              filled
+              type="number"
+              v-model="age"
+              label="Your age *"
+              lazy-rules
+              :rules="[
+                (val) => (val !== null && val !== '') || 'Please type your age',
+                (val) => (val > 0 && val < 100) || 'Please type a real age'
+              ]"
+            />
+
+            <q-toggle v-model="accept" label="I accept the license and terms" />
+
+            <div>
+              <q-btn label="Submit" type="submit" color="primary" />
+              <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+            </div>
+          </q-form>
+        </q-card-section>
+
+        <q-card-actions align="right" class="bg-white text-teal">
+          <q-btn flat label="Cancel" color="primary" v-close-popup />
+          <q-btn flat label="Save" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
