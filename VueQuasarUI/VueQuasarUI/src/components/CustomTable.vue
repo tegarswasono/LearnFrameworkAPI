6a691148e2ce6a4cc55122da09de938ff76a7d53
tegarswasono <tegar.s@weefer.co.id>
2024-06-08 20:01:13 +0700

    checkpoint custom component table done

---
+++ VueQuasarUI/VueQuasarUI/src/components/CustomTable.vue
@@ -0,0 +1,185 @@
+<script setup lang="ts">
+import { onMounted, ref, watch, type Ref } from 'vue'
+import { useQuasar, QForm } from 'quasar'
+
+var props = defineProps({
+  columns: {
+    type: Array<any>,
+    required: true
+  },
+  rows: {
+    type: Array,
+    required: true,
+    default: () => []
+  },
+  loading: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  OnRequest: {
+    type: Function,
+    required: true
+  },
+  onAdd: {
+    type: Function,
+    required: true
+  },
+  onView: {
+    type: Function,
+    required: true
+  },
+  onEdit: {
+    type: Function,
+    required: true
+  },
+  onDelete: {
+    type: Function,
+    required: true
+  },
+  pagination: {
+    type: Object,
+    required: true
+  },
+  dialog: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  onSubmit: {
+    type: Function,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  }
+})
+
+const dialogTitle = ref('Add ' + props.title)
+const visibleSubmit = ref(true)
+// const formRef: Ref<QForm | null> = ref(null)
+const onAddLocal = () => {
+  dialogLocal.value = true
+  dialogTitle.value = 'Add ' + props.title
+  visibleSubmit.value = true
+  props.onAdd()
+}
+const onViewLocal = (value: any) => {
+  dialogLocal.value = true
+  dialogTitle.value = 'View ' + props.title
+  visibleSubmit.value = false
+  props.onView(value)
+}
+const onEditLocal = (value: any) => {
+  dialogLocal.value = true
+  dialogTitle.value = 'Edit ' + props.title
+  visibleSubmit.value = true
+  props.onEdit(value)
+}
+
+// local
+const paginationLocal = ref({ ...props.pagination })
+const dialogLocal = ref(props.dialog)
+watch(
+  () => props.pagination,
+  (newPagination) => {
+    paginationLocal.value = { ...newPagination }
+  },
+  { deep: true }
+)
+const emit = defineEmits(['update:pagination', 'update:dialog'])
+watch(
+  paginationLocal,
+  (newPagination) => {
+    emit('update:pagination', newPagination)
+  },
+  { deep: true }
+)
+watch(
+  () => props.dialog,
+  (newDialog) => {
+    dialogLocal.value = newDialog
+  },
+  { deep: true }
+)
+watch(
+  dialogLocal,
+  (newDialog) => {
+    emit('update:dialog', newDialog)
+  },
+  { deep: true }
+)
+</script>
+
+<template>
+  <!-- Table -->
+  <q-table
+    title="Product"
+    :columns="columns"
+    :rows="rows"
+    row-key="name"
+    separator="cell"
+    v-model:pagination="paginationLocal"
+    dense
+    :loading="loading"
+    @request="(val) => OnRequest(val)"
+  >
+    <template v-slot:top>
+      <q-btn icon="add" size="sm" label="Add" color="secondary" @click="onAddLocal()" />
+    </template>
+    <template v-slot:body-cell-actions="props">
+      <q-td :props="props">
+        <q-btn
+          dense
+          round
+          flat
+          color="grey"
+          size="xs"
+          icon="visibility"
+          @click="onViewLocal(props.row)"
+        ></q-btn>
+        <q-btn
+          dense
+          round
+          flat
+          color="grey"
+          size="xs"
+          icon="edit"
+          @click="onEditLocal(props.row)"
+        ></q-btn>
+        <q-btn
+          dense
+          round
+          flat
+          color="grey"
+          size="xs"
+          icon="delete"
+          @click="onDelete(props.row.id)"
+        ></q-btn>
+      </q-td>
+    </template>
+  </q-table>
+  <!-- DetailView -->
+  <q-dialog v-model="dialogLocal" backdrop-filter="blur(4px) saturate(150%)">
+    <q-card style="width: 700px; max-width: 80vw">
+      <q-card-section>
+        <div class="text-h6">{{ dialogTitle }}</div>
+      </q-card-section>
+      <q-card-section class="q-pt-none">
+        <slot name="detailView"></slot>
+      </q-card-section>
+      <q-card-actions align="right" class="bg-white text-teal">
+        <q-btn label="Close" color="negative" size="sm" icon="cancel" v-close-popup />
+        <q-btn
+          label="Save"
+          color="primary"
+          size="sm"
+          icon="save"
+          @click="onSubmit()"
+          v-if="visibleSubmit"
+        />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
--- VueQuasarUI/VueQuasarUI/src/views/master/product/product-index.vue
+++ VueQuasarUI/VueQuasarUI/src/views/master/product/product-index.vue
@@ -6,21 +6,13 @@ import type { IProductModel, IProductModelCreateOrUpdate } from '@/helpers/api/p
 import { ProductApi } from '@/helpers/api/product/productApi'
 import type { IGeneralDatasourceModel } from '@/helpers/api/datasource/datasourceModel'
 import { DatasourceApi } from '@/helpers/api/datasource/datasourceApi'
+import CustomTable from '@/components/CustomTable.vue'
 
 const quasar = useQuasar()
 const productApi = new ProductApi()
 const datasourceApi = new DatasourceApi()
 
-//listview
 const loading = ref(false)
-const products = ref()
-const pagination = ref({
-  sortBy: 'name',
-  descending: false,
-  page: 1,
-  rowsPerPage: 20,
-  rowsNumber: 0
-})
 const columns: any = [
   { name: 'actions', label: '', align: 'left', style: 'width:50px;' },
   { name: 'name', label: 'Name', field: 'name', sortable: true, align: 'left' },
@@ -34,35 +26,15 @@ const columns: any = [
     align: 'left'
   }
 ]
-const getData = async () => {
-  loading.value = true
-  try {
-    var response = await productApi.getAll(
-      pagination.value.sortBy,
-      pagination.value.descending,
-      pagination.value.page,
-      pagination.value.rowsPerPage
-    )
-    products.value = response.result
-    pagination.value.rowsNumber = response.rowsNumber
-  } catch (error) {
-    /* empty */
-  }
-  loading.value = false
-}
-const OnRequest = async (props: any) => {
-  const { page, rowsPerPage, sortBy, descending } = props.pagination
-  pagination.value.sortBy = sortBy
-  pagination.value.descending = descending
-  pagination.value.page = page
-  pagination.value.rowsPerPage = rowsPerPage
-  await getData()
-}
-
-//Add, Edit, Delete
+const rows = ref()
+const pagination = ref({
+  sortBy: 'name',
+  descending: false,
+  page: 1,
+  rowsPerPage: 20,
+  rowsNumber: 0
+})
 const dialog = ref(false)
-const dialogTitle = ref('Add Product')
-const visibleSubmit = ref(true)
 const formReadonly = ref(false)
 const model: Ref<IProductModelCreateOrUpdate> = ref({
   id: '',
@@ -73,14 +45,35 @@ const model: Ref<IProductModelCreateOrUpdate> = ref({
   categoryId: ''
 })
 const formRef: Ref<QForm | null> = ref(null)
-const categories = ref()
 
+const getData = async () => {
+  loading.value = true
+  try {
+    var response = await productApi.getAll(
+      pagination.value.sortBy,
+      pagination.value.descending,
+      pagination.value.page,
+      pagination.value.rowsPerPage
+    )
+    rows.value = response.result
+    pagination.value.rowsNumber = response.rowsNumber
+  } catch (error) {
+    /* empty */
+  }
+  loading.value = false
+}
+
+const OnRequest = async (props: any) => {
+  const { page, rowsPerPage, sortBy, descending } = props.pagination
+  pagination.value.sortBy = sortBy
+  pagination.value.descending = descending
+  pagination.value.page = page
+  pagination.value.rowsPerPage = rowsPerPage
+  await getData()
+}
 const onAdd = () => {
   dialog.value = true
-  dialogTitle.value = 'Add Product'
-  visibleSubmit.value = true
   formReadonly.value = false
-
   model.value = {
     id: '',
     name: '',
@@ -92,18 +85,12 @@ const onAdd = () => {
 }
 const onView = async (row: any) => {
   dialog.value = true
-  dialogTitle.value = 'View Product'
-  visibleSubmit.value = false
   formReadonly.value = true
-
   model.value = row
 }
 const onEdit = async (row: any) => {
   dialog.value = true
-  dialogTitle.value = 'Edit Product'
-  visibleSubmit.value = true
   formReadonly.value = false
-
   model.value = JSON.parse(JSON.stringify(row))
 }
 const onDelete = async (id: string) => {
@@ -155,6 +142,9 @@ const onSubmit = async () => {
     }
   }
 }
+
+//datasource
+const categories = ref()
 const GetCategories = async () => {
   categories.value = await datasourceApi.category()
 }
@@ -174,137 +164,83 @@ onMounted(async () => {
     <q-breadcrumbs-el label="Product" />
   </q-breadcrumbs>
   <!-- table -->
-  <div>
-    <q-table
-      title="Product"
-      :rows="products"
-      :columns="columns"
-      row-key="name"
-      separator="cell"
-      v-model:pagination="pagination"
-      dense
-      :loading="loading"
-      @request="OnRequest"
-    >
-      <template v-slot:top>
-        <q-btn icon="add" size="sm" label="Add" color="secondary" @click="onAdd" />
-      </template>
-      <template v-slot:body-cell-actions="props">
-        <q-td :props="props">
-          <q-btn
-            dense
-            round
-            flat
-            color="grey"
-            size="xs"
-            icon="visibility"
-            @click="onView(props.row)"
-          ></q-btn>
-          <q-btn
-            dense
-            round
-            flat
-            color="grey"
-            size="xs"
-            icon="edit"
-            @click="onEdit(props.row)"
-          ></q-btn>
-          <q-btn
-            dense
-            round
-            flat
-            color="grey"
-            size="xs"
-            icon="delete"
-            @click="onDelete(props.row.id)"
-          ></q-btn>
-        </q-td>
-      </template>
-    </q-table>
-  </div>
-  <!-- dialog -->
-  <q-dialog v-model="dialog" backdrop-filter="blur(4px) saturate(150%)">
-    <q-card style="width: 700px; max-width: 80vw">
-      <q-card-section>
-        <div class="text-h6">{{ dialogTitle }}</div>
-      </q-card-section>
-
-      <q-form ref="formRef" class="q-gutter-md">
-        <q-card-section class="q-pt-none">
-          <q-input
-            filled
-            v-model="model.name"
-            label="Name *"
-            lazy-rules
-            dense
-            maxlength="100"
-            :rules="[(val) => (val && val.length > 0) || 'Name is required']"
-            :readonly="formReadonly"
-          />
-          <q-input
-            type="number"
-            filled
-            v-model="model.stok"
-            label="Stok *"
-            lazy-rules
-            dense
-            :rules="[
-              (val) => val !== '' || 'Stok is required',
-              (val) => val >= 0 || 'Stok Should be bigger than or equals to zero'
-            ]"
-            :readonly="formReadonly"
-          />
-          <q-input
-            type="number"
-            filled
-            v-model="model.price"
-            label="Price *"
-            lazy-rules
-            dense
-            :rules="[
-              (val) => val !== '' || 'Price is required',
-              (val) => val > 0 || 'Price Should be bigger than zero'
-            ]"
-            :readonly="formReadonly"
-          />
-          <q-select
-            filled
-            dense
-            v-model="model.categoryId"
-            label="Category"
-            :options="categories"
-            clearable
-            emit-value
-            map-options
-            :readonly="formReadonly"
-            lazy-rules
-            :rules="[(val) => (val && val.length > 0) || 'Category is required']"
-          />
-          <br />
-          <q-input
-            type="textarea"
-            filled
-            v-model="model.description"
-            label="Description"
-            lazy-rules
-            dense
-            maxlength="300"
-            :readonly="formReadonly"
-            class="textarea-no-resize"
-          />
-        </q-card-section>
-        <q-card-actions align="right" class="bg-white text-teal">
-          <q-btn label="Close" color="negative" size="sm" icon="cancel" v-close-popup />
-          <q-btn
-            label="Save"
-            color="primary"
-            size="sm"
-            icon="save"
-            :onclick="onSubmit"
-            v-if="visibleSubmit"
-          />
-        </q-card-actions>
+  <CustomTable
+    :columns="columns"
+    :rows="rows"
+    :loading="loading"
+    :OnRequest="OnRequest"
+    :onAdd="onAdd"
+    :onView="onView"
+    :onEdit="onEdit"
+    :onDelete="onDelete"
+    :pagination="pagination"
+    :dialog="dialog"
+    :onSubmit="onSubmit"
+    title="Product"
+  >
+    <template #detailView>
+      <q-form class="q-gutter-md" ref="formRef">
+        <q-input
+          filled
+          v-model="model.name"
+          label="Name *"
+          lazy-rules
+          dense
+          maxlength="100"
+          :rules="[(val) => (val && val.length > 0) || 'Name is required']"
+          :readonly="formReadonly"
+        />
+        <q-input
+          type="number"
+          filled
+          v-model="model.stok"
+          label="Stok *"
+          lazy-rules
+          dense
+          :rules="[
+            (val) => val !== '' || 'Stok is required',
+            (val) => val >= 0 || 'Stok Should be bigger than or equals to zero'
+          ]"
+          :readonly="formReadonly"
+        />
+        <q-input
+          type="number"
+          filled
+          v-model="model.price"
+          label="Price *"
+          lazy-rules
+          dense
+          :rules="[
+            (val) => val !== '' || 'Price is required',
+            (val) => val > 0 || 'Price Should be bigger than zero'
+          ]"
+          :readonly="formReadonly"
+        />
+        <q-select
+          filled
+          dense
+          v-model="model.categoryId"
+          label="Category"
+          :options="categories"
+          clearable
+          emit-value
+          map-options
+          :readonly="formReadonly"
+          lazy-rules
+          :rules="[(val) => (val && val.length > 0) || 'Category is required']"
+        />
+        <q-input
+          type="textarea"
+          filled
+          v-model="model.description"
+          label="Description"
+          lazy-rules
+          dense
+          maxlength="300"
+          :readonly="formReadonly"
+          class="textarea-no-resize"
+        />
       </q-form>
-    </q-card>
-  </q-dialog>
+    </template>
+  </CustomTable>
 </template>
